ecff4d5df014cc85d641240049383378ed0cc36f
sinkthetime <npreindeer@gmail.com>
2016-06-08 10:00:44 -0700

    wednesday final

--- APCSFinal/src/APCSFinal/TileRunner.java
+++ APCSFinal/src/APCSFinal/TileRunner.java
@@ -7,23 +7,13 @@ import javax.swing.SwingUtilities;
 public class TileRunner {
         public static final int WIDTH = 512;
         public static final int HEIGHT = 512;
-       
         public static JFrame gui;
-       
         public static void main(String[] args) {
-                SwingUtilities.invokeLater(new Runnable() {
-                       
-                        @Override
-                        public void run() {
-                                gui = new JFrame("Sliding Puzzle");
-                                gui.setUndecorated(true);
-                                gui.setSize(WIDTH, HEIGHT);
-                               
-                                TileGui tilo = new TileGui();
-                                gui.add(tilo);
-                               
-                                gui.setVisible(true);
-                        }
-                });
+            gui = new JFrame("Sliding Puzzle");
+            gui.setUndecorated(true);
+            gui.setSize(WIDTH, HEIGHT);
+            TileGui tilo = new TileGui();
+            gui.add(tilo);
+            gui.setVisible(true);
         }
 }
